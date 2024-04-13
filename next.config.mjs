ecca061b814290ab0253b9bbd857a5aa7a83d090
Jahid Uddin <jahidudd65@gmail.com>
2024-04-13 17:19:46 +0200

    add env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
+        EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID,
+        EMAIL_PUBLIC_KEY: process.env.EMAIL_PUBLIC_KEY
+    }
+};
 
 export default nextConfig;
